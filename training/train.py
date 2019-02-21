bc216cbeb7865c7496ddca8637cbf3a5248943d0
Ravindra Singh Ratnawat <rratnawa@redhat.com>
2019-02-21 22:39:30 +0530

    Update train.py

--- training/train.py
+++ training/train.py
@@ -1,6 +1,14 @@
 print("HELLO WORLD")
-import os, json
 
-print(os.getenv('AWS_S3_ACCESS_KEY_ID'))
-print(os.getenv('HYPER_PARAMS'))
-print(json.loads(os.getenv('HYPER_PARAMS')))
+from rudra import logger
+def load_hyper_params():
+    """Load the hyper parameter from the command line args."""
+    if len(argv) > 1:
+        input_data = argv[1:]
+        try:
+            if input_data:
+                return loads(input_data[0])
+        except Exception as exc:
+            logger.error("Unable to decode the hyper params")
+            
+print(load_hyper_params())
